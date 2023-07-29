c496f794e46cc3b2278d963fcff1b35e25b1542d
naderkamelaponar <nader.kamel.the.programmer@gmail.com>
2023-07-29 23:31:51 +0300

    fix

--- src/constant/env.js
+++ src/constant/env.js
@@ -1,3 +1,3 @@
-export const SERVER_URL = "https://tronweb-front.onrender.com/"
+export const SERVER_URL = "https://tronweb.onrender.com/"
 // export const SERVER_URL = "http://10.10.12.157:5000/api/"
 
